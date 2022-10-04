454c38815284c244efdf9389d999174856def813
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2022-10-04 13:00:29 +0400

    add arion

--- conf/configuration.nix
+++ conf/configuration.nix
@@ -35,5 +35,19 @@
       neovim
       curl
       wget
-  ];
+      arion
+
+      # Do install the docker CLI to talk to podman.
+      # Not needed when virtualisation.docker.enable = true; 
+      docker-client
+    ];
+
+  # Arion works with Docker, but for NixOS-based containers, you need Podman
+  # since NixOS 21.05.
+  virtualisation.docker.enable = false;
+  virtualisation.podman.enable = true;
+  virtualisation.podman.dockerSocket.enable = true;
+  virtualisation.podman.defaultNetwork.dnsname.enable = true;
+
+  users.extraUsers.root.extraGroups = ["podman"];
 }
